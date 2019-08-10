849b3d0ed83934645a2fd0a24078b336ec3561c3
joelerego <joelerego@outlook.com>
2019-08-10 20:56:22 +0530

    Usage of conditionals, memory read and disassembly

---
+++ conditionals/conditionals/Log.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+
+void Log(const char* message)
+{
+	std::cout << message << std::endl;
+}
---
+++ conditionals/conditionals/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Log.h"
+
+int main()
+{
+	int x = 5;
+	bool comparisonResult = x == 5;
+	if (comparisonResult) 
+	{
+		Log("Hello World");
+	}
+	
+	std::cin.get();
+	return 0;
+}
